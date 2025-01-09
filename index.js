f6ee84b07341181ee8ed4fa1984abf820b0a8c4e
Roman-Glyshanitsya <79175208+Roman-Glyshanitsya@users.noreply.github.com>
2025-01-09 16:10:08 +0000

    Deploying to gh-pages from @ Roman-Glyshanitsya/event-booster@9127aa1a8a67852ce690b014387ad73b59ac6a06 🚀

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var p=s=>{throw TypeError(s)};var g=(s,e,t)=>e.has(s)||p("Cannot "+t);var l=(s,e,t)=>(g(s,e,"read from private field"),t?t.call(s):e.get(s)),d=(s,e,t)=>e.has(s)?p("Cannot add the same private member more than once"):e instanceof WeakSet?e.add(s):e.set(s,t);(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))o(r);new MutationObserver(r=>{for(const n of r)if(n.type==="childList")for(const i of n.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function t(r){const n={};return r.integrity&&(n.integrity=r.integrity),r.referrerPolicy&&(n.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?n.credentials="include":r.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function o(r){if(r.ep)return;r.ep=!0;const n=t(r);fetch(r.href,n)}})();var c,a;class v{constructor(){d(this,c,"cbbvEGI4A6Q7cSGEi3Xqg23Vkvb9pPby");d(this,a,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=1,this.pageSize=8}async fetchEvents(){const e=`${l(this,a)}?apikey=${l(this,c)}&size=${this.pageSize}&page=${this.page}&classificationName=music`,t=await fetch(e);if(!t.ok)throw new Error("Faild to fetch events");return t.json()}}c=new WeakMap,a=new WeakMap;function h(s){return s.map(t=>{var u,m,f;const o=((u=t.images[1])==null?void 0:u.url)||"/img/default-image.png",r=t.name||"No Name Available",n=t.dates.start.localDate||"Unknown Date",i=((f=(m=t._embedded)==null?void 0:m.venues[0])==null?void 0:f.name)||"Unknown Location",_=t.url||"#";return`
+var p=e=>{throw TypeError(e)};var y=(e,t,r)=>t.has(e)||p("Cannot "+r);var l=(e,t,r)=>(y(e,t,"read from private field"),r?r.call(e):t.get(e)),u=(e,t,r)=>t.has(e)?p("Cannot add the same private member more than once"):t instanceof WeakSet?t.add(e):t.set(e,r);(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const n of s)if(n.type==="childList")for(const c of n.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function r(s){const n={};return s.integrity&&(n.integrity=s.integrity),s.referrerPolicy&&(n.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?n.credentials="include":s.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function o(s){if(s.ep)return;s.ep=!0;const n=r(s);fetch(s.href,n)}})();var a,i;class b{constructor(){u(this,a,"cbbvEGI4A6Q7cSGEi3Xqg23Vkvb9pPby");u(this,i,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=1,this.pageSize=12,this.searchQuery=""}async fetchEvents(){const t=new URLSearchParams({apikey:l(this,a),keyword:this.searchQuery,page:this.page,size:this.pageSize}),r=`${l(this,i)}?${t}&classificationName=music&`,o=await fetch(r);if(!o.ok)throw new Error("Faild to fetch events");return o.json()}}a=new WeakMap,i=new WeakMap;function E(e){return e.map(r=>{var d,m,f;const o=((d=r.images[0])==null?void 0:d.url)||"/img/default-image.png",s=r.name||"No Name Available",n=r.dates.start.localDate||"Unknown Date",c=((f=(m=r._embedded)==null?void 0:m.venues[0])==null?void 0:f.name)||"Unknown Location",g=r.url||"#";return`
         <li class="cards__item">
         <img
           src="${o}"
-          alt="${r}"
+          alt="${s}"
           class="cards__item__image"
         />
-        <h2 class="cards__item__title">${r}</h2>
+        <h2 class="cards__item__title">${s}</h2>
         <p class="cards__item__date">${n}</p>
         <a
-          href="${_}"
+          href="${g}"
           class="cards__item__location__link"
           target="_blank"
           rel="noopener noreferrer"
@@ -16,8 +16,8 @@ var p=s=>{throw TypeError(s)};var g=(s,e,t)=>e.has(s)||p("Cannot "+t);var l=(s,e
           <svg class="cards__item__location__icon">
             <use href="/img/icons/symbol-defs.svg#icon-location"></use>
           </svg>
-          ${i}</a
+          ${c}</a
         >
       </li>
-      `}).join("")}const y=document.querySelector(".cards__list"),b=new v;async function E(){var s;try{const t=((s=(await b.fetchEvents())._embedded)==null?void 0:s.events)||[],o=h(t);y.insertAdjacentHTML("beforeend",o)}catch(e){console.error("Error rendering events: ",e)}}E();
+      `}).join("")}const h=document.querySelector(".cards__list"),L=document.querySelector(".header__form"),w=document.getElementById("searchQuery"),_=new b;v();L.addEventListener("submit",S);async function S(e){e.preventDefault(),_.searchQuery=w.value.trim(),A(),await v()}async function v(){var e;try{const r=((e=(await _.fetchEvents())._embedded)==null?void 0:e.events)||[],o=E(r);h.insertAdjacentHTML("beforeend",o)}catch(t){console.error("Error rendering events: ",t)}}function A(){h.innerHTML=""}
 //# sourceMappingURL=index.js.map
