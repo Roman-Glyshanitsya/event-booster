812a989830b165b8cd2901e887eaf832b1c2182c
Roman-Glyshanitsya <79175208+Roman-Glyshanitsya@users.noreply.github.com>
2025-01-14 16:24:05 +0000

    Deploying to gh-pages from @ Roman-Glyshanitsya/event-booster@c28b2f0c3c721bd212fa785c89b2ea030471784a 🚀

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var p=e=>{throw TypeError(e)};var y=(e,t,r)=>t.has(e)||p("Cannot "+r);var l=(e,t,r)=>(y(e,t,"read from private field"),r?r.call(e):t.get(e)),u=(e,t,r)=>t.has(e)?p("Cannot add the same private member more than once"):t instanceof WeakSet?t.add(e):t.set(e,r);(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const n of s)if(n.type==="childList")for(const c of n.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function r(s){const n={};return s.integrity&&(n.integrity=s.integrity),s.referrerPolicy&&(n.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?n.credentials="include":s.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function o(s){if(s.ep)return;s.ep=!0;const n=r(s);fetch(s.href,n)}})();var a,i;class b{constructor(){u(this,a,"cbbvEGI4A6Q7cSGEi3Xqg23Vkvb9pPby");u(this,i,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=1,this.pageSize=12,this.searchQuery=""}async fetchEvents(){const t=new URLSearchParams({apikey:l(this,a),keyword:this.searchQuery,page:this.page,size:this.pageSize}),r=`${l(this,i)}?${t}&classificationName=music&`,o=await fetch(r);if(!o.ok)throw new Error("Faild to fetch events");return o.json()}}a=new WeakMap,i=new WeakMap;function E(e){return e.map(r=>{var d,m,f;const o=((d=r.images[0])==null?void 0:d.url)||"/img/default-image.png",s=r.name||"No Name Available",n=r.dates.start.localDate||"Unknown Date",c=((f=(m=r._embedded)==null?void 0:m.venues[0])==null?void 0:f.name)||"Unknown Location",g=r.url||"#";return`
+var h=e=>{throw TypeError(e)};var L=(e,t,r)=>t.has(e)||h("Cannot "+r);var d=(e,t,r)=>(L(e,t,"read from private field"),r?r.call(e):t.get(e)),m=(e,t,r)=>t.has(e)?h("Cannot add the same private member more than once"):t instanceof WeakSet?t.add(e):t.set(e,r);(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const n of document.querySelectorAll('link[rel="modulepreload"]'))o(n);new MutationObserver(n=>{for(const s of n)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&o(a)}).observe(document,{childList:!0,subtree:!0});function r(n){const s={};return n.integrity&&(s.integrity=n.integrity),n.referrerPolicy&&(s.referrerPolicy=n.referrerPolicy),n.crossOrigin==="use-credentials"?s.credentials="include":n.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(n){if(n.ep)return;n.ep=!0;const s=r(n);fetch(n.href,s)}})();var l,u;class E{constructor(){m(this,l,"cbbvEGI4A6Q7cSGEi3Xqg23Vkvb9pPby");m(this,u,"https://app.ticketmaster.com/discovery/v2/events.json");this.page=0,this.pageSize=12,this.searchQuery=""}async fetchEvents(){const t=new URLSearchParams({apikey:d(this,l),keyword:this.searchQuery,page:this.page,size:this.pageSize}),r=`${d(this,u)}?${t}&classificationName=music`,o=await fetch(r);if(!o.ok)throw new Error("Faild to fetch events");return o.json()}}l=new WeakMap,u=new WeakMap;function w(e){return e.map(r=>{var p,g,_;const o=((p=r.images[0])==null?void 0:p.url)||"/img/default-image.png",n=r.name||"No Name Available",s=r.dates.start.localDate||"Unknown Date",a=((_=(g=r._embedded)==null?void 0:g.venues[0])==null?void 0:_.name)||"Unknown Location",c=r.url||"#";return`
         <li class="cards__item">
         <img
           src="${o}"
-          alt="${s}"
+          alt="${n}"
           class="cards__item__image"
         />
-        <h2 class="cards__item__title">${s}</h2>
-        <p class="cards__item__date">${n}</p>
+        <h2 class="cards__item__title">${n}</h2>
+        <p class="cards__item__date">${s}</p>
         <a
-          href="${g}"
+          href="${c}"
           class="cards__item__location__link"
           target="_blank"
           rel="noopener noreferrer"
@@ -16,8 +16,14 @@ var p=e=>{throw TypeError(e)};var y=(e,t,r)=>t.has(e)||p("Cannot "+r);var l=(e,t
           <svg class="cards__item__location__icon">
             <use href="/img/icons/symbol-defs.svg#icon-location"></use>
           </svg>
-          ${c}</a
+          ${a}</a
         >
       </li>
-      `}).join("")}const h=document.querySelector(".cards__list"),L=document.querySelector(".header__form"),w=document.getElementById("searchQuery"),_=new b;v();L.addEventListener("submit",S);async function S(e){e.preventDefault(),_.searchQuery=w.value.trim(),A(),await v()}async function v(){var e;try{const r=((e=(await _.fetchEvents())._embedded)==null?void 0:e.events)||[],o=E(r);h.insertAdjacentHTML("beforeend",o)}catch(t){console.error("Error rendering events: ",t)}}function A(){h.innerHTML=""}
+      `}).join("")}function v(e,t,r){const o=document.querySelector(".cards__pagination-list");o.innerHTML="";let n="";for(let a=0;a<e;a++)n+=`
+        <li class="pagination__item">
+        <button class="pagination__button ${a===t?"active":""}" data-page="${a}">
+        ${a+1}
+        </button>
+        </li>
+        `;o.insertAdjacentHTML("beforeend",n),o.addEventListener("click",s);function s(a){if(a.target.tagName!=="BUTTON")return;const c=Number(a.target.dataset.page);c!==t&&r(c)}}console.log(v);const y=document.querySelector(".cards__list"),P=document.querySelector(".header__form"),k=document.getElementById("searchQuery"),i=new E;f();P.addEventListener("submit",S);async function S(e){e.preventDefault(),i.searchQuery=k.value.trim(),b(),await f()}async function f(){var e;try{const r=((e=(await i.fetchEvents())._embedded)==null?void 0:e.events)||[],o=w(r);y.insertAdjacentHTML("beforeend",o),v(i.totalPages,i.page,$)}catch(t){console.error("Error rendering events: ",t)}}function b(){y.innerHTML=""}function $(){i.page=newPage,b(),f()}
 //# sourceMappingURL=index.js.map
